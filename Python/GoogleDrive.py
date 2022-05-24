2b14fd6a0ec1453bed2e33da606439f700efb58a
Syama Mishra <38512086+SyamaMishra@users.noreply.github.com>
2022-05-24 23:10:25 +1000

    Added google drive service account mode

--- Python/GoogleDrive.py
+++ Python/GoogleDrive.py
@@ -13,37 +13,44 @@
 
 #Alt method
 # https://stackoverflow.com/questions/60736955/how-to-connect-pydrive-with-an-service-account
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 from oauth2client.service_account import ServiceAccountCredentials
 
 JSON_FILE = "client_secrets.json"
 gauth = GoogleAuth()
 scope = ["https://www.googleapis.com/auth/drive"]
+gauth.auth_method = 'service'
 gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(JSON_FILE, scope)
 drive = GoogleDrive(gauth)
 
+about = drive.GetAbout()
+
+print('Current user name:{}'.format(about['name']))
+print('Root folder ID:{}'.format(about['rootFolderId']))
+print('Total quota (bytes):{}'.format(about['quotaBytesTotal']))
+print('Used quota (bytes):{}'.format(about['quotaBytesUsed']))
 
 #Upload file.
 #https://stackoverflow.com/questions/46562255/python-upload-my-own-files-into-my-drive-using-pydrive-library
 
-myfile = "SyamaFile.txt"
-file1 = drive.CreateFile()
-#Alt - Manually overwrite metadata
-# file1 = drive.CreateFile(metadata={"title": "CustomFileName.txt"})
+# myfile = "SyamaFile.txt"
+# file1 = drive.CreateFile()
+# #Alt - Manually overwrite metadata
+# # file1 = drive.CreateFile(metadata={"title": "CustomFileName.txt"})
 
-file1.SetContentFile(myfile)
-file1.Upload()
+# file1.SetContentFile(myfile)
+# file1.Upload()
 
-#https://pythonhosted.org/PyDrive/filemanagement.html
-# Insert the permission.
-permission = file1.InsertPermission({
-                        'type': 'anyone',
-                        'value': 'anyone',
-                        'role': 'reader'})
+# #https://pythonhosted.org/PyDrive/filemanagement.html
+# # Insert the permission.
+# permission = file1.InsertPermission({
+#                         'type': 'anyone',
+#                         'value': 'anyone',
+#                         'role': 'reader'})
 
-print(file1['permissions'])
-print(file1['alternateLink'])  # Display the sharable link.
+# print(file1['permissions'])
+# print(file1['alternateLink'])  # Display the sharable link.
 
 #List files, simple.
 #https://pythonhosted.org/PyDrive/filelist.html
@@ -60,3 +67,18 @@ print(file1['alternateLink'])  # Display the sharable link.
 #   print('Received %s files from Files.list()' % len(file_list)) # <= 10
 #   for file1 in file_list:
 #       print('title: %s, id: %s' % (file1['title'], file1['id']))
+
+#List teamdrive contents
+#https://stackoverflow.com/questions/43243865/how-to-access-team-drive-using-service-account-with-google-drive-net-api-v3
+
+teamdriveid = "0AAxYOGm9eaU2Uk9PVA"
+folderid = "15iLIEXBfNQnBcvv_MyA8rdaYbA7XcDhB"
+file_list = drive.ListFile({
+    'q': f"'{folderid}' in parents",
+    'supportsAllDrives': True,  # Modified
+    'driveId': teamdriveid,  # Modified
+    'includeItemsFromAllDrives': True,  # Added
+    'corpora': 'drive'  # Added
+}).GetList()
+for file1 in file_list:
+    print('title: %s, id: %s' % (file1['title'], file1['id']))
